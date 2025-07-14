88079f2616b62dbe54bea676288b5becf813e9f9
64MM4-KN1F3 <aweatherley@gmail.com>
2025-07-14 16:10:02 +1000

    add finishing touches (hopefully)

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://64mm4-kn1f3.github.io/D473',
+  base: '/',
+});
